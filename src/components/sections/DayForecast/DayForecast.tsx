4b686149db263fcc1ebd5d12434d2c545d77f0fb
Dmitry Draganov <draqanovdfeet@gmail.com>
2025-07-24 21:27:58 +0300

    made data array for chart

--- src/components/sections/DayForecast/DayForecast.tsx
+++ src/components/sections/DayForecast/DayForecast.tsx
@@ -1,7 +1,44 @@
 import styles from './DayForecast.module.scss';
 import SectionHeading from '../../SectionHeading/SectionHeading';
+import { useContext, useEffect } from 'react';
+import APIContext from '../../../contexts/APIContext';
+import { getWeatherIcon } from '../../../utilities/iconMapper';
+
+interface IChartData {
+	time: string;
+	temp: number;
+	conditionIcon: string;
+	windSpeed: number;
+}
 
 const DayForecast = () => {
+	const daysForecasts = useContext(APIContext)!.forecast.forecastday;
+	const hoursForecast = [...daysForecasts[0].hour, ...daysForecasts[1].hour];
+
+	const currentTime = new Date().getHours();
+	let chartData: IChartData[];
+
+	useEffect(() => {
+		chartData = hoursForecast
+			.filter(
+				(hourForecast, index) =>
+					index <= currentTime + 23 && index >= currentTime
+			)
+			.map(hourForecast => {
+				const hour = new Date(hourForecast.time_epoch * 1000).getHours();
+				return {
+					time: `${hour}:00`,
+					temp: hourForecast.temp_c,
+					conditionIcon: getWeatherIcon(hourForecast.condition.code),
+					windSpeed: hourForecast.wind_kph,
+				};
+			});
+
+		chartData[0].time = 'Now';
+
+		console.log(chartData);
+	}, [hoursForecast, currentTime]);
+
 	return (
 		<section className={`division flex-container__flex-item`}>
 			<SectionHeading iconID='clock' text='24-hours forecast' />
